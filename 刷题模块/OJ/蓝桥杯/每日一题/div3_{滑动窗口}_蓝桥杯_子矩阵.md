1cb3045ff4cfe475490ccbe3f0c9211d30fc94e4
AliceAuto <sgq2742798794@gmail.com>
2025-01-22 19:43:30 +0800

    h

---
+++ 刷题模块/OJ/蓝桥杯/每日一题/div3_{滑动窗口}_蓝桥杯_子矩阵.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "子矩阵"
+permalink: /刷题模块/OJ/蓝桥杯/每日一题/div3_{滑动窗口}_蓝桥杯_子矩阵.md/
+date: 2025-01-22
+author: "JNMC孙国庆"
+---
+
+#### [备用返回通道](../../README.md)
+# [转到题目](https://www.lanqiao.cn/problems/3521/learning/?page=1&first_category_id=1&name=%E5%AD%90%E7%9F%A9%E9%98%B5)
+## 思路解析
--- 算法/板子/手速练习.cpp
+++ 算法/板子/手速练习.cpp
@@ -1,19 +1,1 @@
-#include<bits/stdc++.h>
 
-int main()
-{
-	std::vector<int> vec;
-
-	std::string inp;
-	getline (std::cin,inp);
-	std::istringstream Is (inp);
-	int tem;
-	while(Is>>tem)vec.push_back(tem);
-	std::vector<double> vvec;
-	std::transform(vec.begin(),vec.end(),std::back_inserter(vvec),[](int x){return static_cast<double> (x*0.1) ;});
-	
-	for(auto x: vvec){
-		std::cout<<std::fixed<<std::setprecision(2)<<x<<" ";
-	}
-	return 0;
-}
